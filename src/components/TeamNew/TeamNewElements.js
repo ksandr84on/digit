0c799dc4743143fbec9f60a471b0d5ccd346d42d
ksandron <zandrewboyle@vk.com>
2023-03-14 02:06:52 +0300

    upd

--- src/components/TeamNew/TeamNewElements.js
+++ src/components/TeamNew/TeamNewElements.js
@@ -65,9 +65,13 @@ export const TopLine = styled.p`
 export const Heading = styled.h1`
   margin-bottom: 24px;
   font-size: 48px;
-  line-height: 1.1;
-  font-weight: 600;
+
   color: ${({lightText}) => (lightText ? '#f7f8fa' : '#010606')};
+
+  @media screen and (max-width: 768px) {
+    font-size: 32px;
+  }
+
 
   @media screen and (max-width: 480px) {
     font-size: 32;
